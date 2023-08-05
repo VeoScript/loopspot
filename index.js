3edde23c761b094eb97dfecc8bdd0a659b10206d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-08-05 14:14:55 +0800

    feat: remove text and text-input scaling

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+if (Text.defaultProps == null) {
+  Text.defaultProps = Text.defaultProps || {};
+  Text.defaultProps.allowFontScaling = false;
+}
+
+if (TextInput.defaultProps == null) {
+  TextInput.defaultProps = {};
+  TextInput.defaultProps.allowFontScaling = false;
+  TextInput.defaultProps.autoComplete = 'off';
+}
 
 AppRegistry.registerComponent(appName, () => App);
