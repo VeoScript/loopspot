522b9ea288a6b3e5f5a722204e28c7a1dd25e8f0
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-08-06 15:19:27 +0800

    feat: add main screens and other components (#7)
    * feat: add main screens and other components
    
    * feat: add important components
    
    - TopBar
    - BottomBar
    - Icons
    - Toast

---
+++ src/config/AppStacks.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StatusBarMain from '../components/organisms/StatusBarMain';
+import * as screen from '../shared/screens';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {navigationRef} from './RootNavigation';
+import {useGuard} from '../lib/hooks/useGuard';
+
+const Stack = createNativeStackNavigator();
+
+const AppStacks = () => {
+  const isAuth = useGuard();
+
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <StatusBarMain
+        animated={false}
+        backgroundColor="#FFE1A8"
+        barStyle="dark-content"
+      />
+      <Stack.Navigator screenOptions={{headerShown: false, animation: 'none'}}>
+        {!isAuth ? (
+          <>
+            <Stack.Screen name="LoginScreen" component={screen.LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={screen.RegisterScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="HomeScreen" component={screen.HomeScreen} />
+            <Stack.Screen name="NotificationScreen" component={screen.NotificationScreen} />
+            <Stack.Screen name="SearchScreen" component={screen.SearchScreen} />
+            <Stack.Screen name="InspiredScreen" component={screen.InspiredScreen} />
+            <Stack.Screen name="ProfileScreen" component={screen.ProfileScreen} />
+            <Stack.Screen name="CreatePostScreen" component={screen.CreatePostScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppStacks;
