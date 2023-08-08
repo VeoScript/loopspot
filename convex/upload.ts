0df24e945063f919685d4352a7bda5c9000445cd
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-08-08 22:02:03 +0800

    feat: add covers table for cover photo

--- convex/upload.ts
+++ convex/upload.ts
@@ -19,6 +19,24 @@ export const profilePhoto = query({
   },
 });
 
+export const coverPhoto = query({
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const cover = await ctx.db
+      .query('covers')
+      .filter(q => q.eq(q.field('authorId'), args.userId))
+      .unique();
+
+    return {
+      _id: cover?._id,
+      authorId: cover?.authorId,
+      url: cover?.format === 'image' ? await ctx.storage.getUrl(cover.body) : ''
+    }
+  },
+});
+
 export const generateUploadUrl = mutation(async ctx => {
   return await ctx.storage.generateUploadUrl();
 });
@@ -38,6 +56,26 @@ export const updateProfileImage = mutation({
   args: {profileId: v.id("profiles"), storageId: v.string()},
   handler: async (ctx, args) => {
     await ctx.db.patch(args.profileId, {
+      body: args.storageId,
+    });
+  },
+});
+
+export const sendCoverImage = mutation({
+  args: {storageId: v.string(), authorId: v.string()},
+  handler: async (ctx, args) => {
+    await ctx.db.insert('covers', {
+      body: args.storageId,
+      authorId: args.authorId,
+      format: 'image',
+    });
+  },
+});
+
+export const updateCoverImage = mutation({
+  args: {coverId: v.id("covers"), storageId: v.string()},
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.coverId, {
       body: args.storageId,
     });
   },
