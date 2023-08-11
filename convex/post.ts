785a5133859231c141e5bd470490776d130444e3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-08-11 22:47:18 +0800

    feat: add get post functions in convex

--- convex/post.ts
+++ convex/post.ts
@@ -5,6 +5,27 @@ export const generateUploadUrl = mutation(async ctx => {
   return await ctx.storage.generateUploadUrl();
 });
 
+export const post = query({
+  args: {
+    postId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const post = await ctx.db.query("posts")
+    .filter((q) => q.eq(q.field("_id"), args.postId))
+    .unique();
+
+    return {
+      _id: post?._id,
+      _creationTime: post?._creationTime,
+      title: post?.title,
+      description: post?.description,
+      article: post?.article,
+      authorId: post?.authorId,
+      url: post?.format === 'image' ? await ctx.storage.getUrl(post.storageId) : ''
+    }
+  },
+});
+
 export const posts = query({
   args: {
     userId: v.string(),
@@ -13,6 +34,7 @@ export const posts = query({
     const posts = await ctx.db
       .query('posts')
       .filter(q => q.eq(q.field('authorId'), args.userId))
+      .order('desc')
       .collect();
 
     return Promise.all(
